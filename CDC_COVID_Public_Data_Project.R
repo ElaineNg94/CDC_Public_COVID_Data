f8e18464fdc7923f200d302038ee084c89050067
Elaine Ng <Elaine.Ng246@gmail.com>
2022-02-16 19:28:04 -0800

    Delete CDC_COVID_Public_Data_Project.R

--- CDC_COVID_Public_Data_Project.R
+++
@@ -1,6 +0,0 @@
-#load dplyr using library()
-library('dplyr')
-#install.packages('dplyr')
-
-#Import and read in the COVID-19_Case_Surveillance_Public_Use_Data.csv file as a dataframe. (15.2.3)
-COVID <- read.csv(file = 'COVID-19_Case_Surveillance_Public_Use_Data.csv',check.names=F, stringsAsFactors = F)
